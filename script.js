e18104356a4bc2ae5af615055c86114fafc89402
PikriaP <ffilishvili@gmail.com>
2024-09-20 12:10:41 +0400

    Elene is in the persons array

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ console.log(reverced);
 const studentsWithA= studensts.filter(word => word.includes('a'));
 console.log(studentsWithA);
 // [ 'danieli', 'Marta', 'Mariami', 'Natia' ]
+
+// 3
+const persons = ['David', 'Elene', 'Teona', 'Giorgi'];
+const hasElene =persons.includes('Elene');
+if(hasElene) {
+    console.log('Elene is in the persons array');
+
+}else{
+    console.log('Elene is not in the persons array.');
+    
+}
+// Elene is in the persons array
+
